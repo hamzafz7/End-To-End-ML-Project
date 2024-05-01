77235f28f610fccc09ef3f61befffe02e710cf6b
hamzafz7 <hamzafz888@gmail.com>
2024-05-02 00:40:34 +0300

    set up and requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name='ML_Project_Structure' ,
+    version='0.0.1' , 
+    author= 'Hamza' ,
+    author_email="hamzafz888@gmail.com" ,
+    packages=find_packages(), 
+    
+
+
+    
+    
+)
